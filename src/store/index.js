9903cbacf692b0ee0964a9220b3644e8ce153b29
reidfleites <dreinier86@gmail.com>
2022-04-20 07:24:09 +0200

    done

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,29 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    msg:"Vuex desde cero.",
+    nombre: "Reinier",
+    apellido : "Diaz",
+    amigos:[],
+    amigo:null
   },
   getters: {
+    message(state){
+      return state.msg;
+    },
+    nombreCompleto(state){
+      return `${state.nombre} ${state.apellido}`;
+    }
   },
   mutations: {
+    addAmigo(state){
+      state.amigos = [...state.amigos,state.amigo]
+    }
   },
   actions: {
+    addAmigoAction(context){
+      context.commit('addAmigo');
+    }
   },
   modules: {
   }
